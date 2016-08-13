2ab7d00493f7dae0dce6b6d4085cf4edba5ad08e
arnif <arni@takumi.com>
2016-08-13 07:59:58 +0000

    Adding Ultra Rare

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const moment = require('moment');
 const axios = require('axios');
 const geocoder = require('geocoder');
 
-const PUSHOVER_USERS = process.env['PUSHOVER_USERS'].split(',');
-const PUSHOVER_TOKEN = process.env['PUSHOVER_TOKEN'];
+const PUSHOVER_USERS = process.env['POKEMON_PUSHOVER_USERS'].split(',');
+const PUSHOVER_TOKEN = process.env['POKEMON_PUSHOVER_TOKEN'];
 
 const url = 'https://pokemap.haukur.io/raw_data?pokemon=true&pokestops=true&gyms=false&scanned=false&swLat=64.08783448918172&swLng=-22.10130761988205&neLat=64.1597555893088&neLng=-21.60692285425705&_=1470823652310'
 
@@ -26,7 +26,7 @@ function main() {
   console.log('looking');
   axios(config).then((response) => {
     response.data.pokemons.map((pokemon) => {
-      if (pokemon.pokemon_rarity === 'Very Rare') {
+      if (pokemon.pokemon_rarity === 'Very Rare' || pokemon.pokemon_rarity === 'Ultra Rare') {
         console.log('alreadyNotified.indexOf(pokemon.encounter_id)', alreadyNotified.indexOf(pokemon.encounter_id));
         if (alreadyNotified.indexOf(pokemon.encounter_id) < 0) {
 
