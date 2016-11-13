f49a0610360c5a7bdc92d03f3851191f294d4bad
arnif <arni@takumi.com>
2016-11-13 16:58:54 +0000

    Remove two dead sites

--- index.js
+++ index.js
@@ -17,8 +17,8 @@ var client = new Twitter({
 });
 
 const urls = [
-  'https://pokemap.haukur.io/raw_data?pokemon=true&pokestops=false&gyms=false&scanned=false&spawnpoints=false&swLat=64.07643930614307&swLng=-22.145296709082004&neLat=64.19042456941561&neLng=-21.55958809091794&_=1473462800117',
-  'http://pogomap.1337.is/raw_data?pokemon=true&pokestops=false&gyms=false&scanned=false&spawnpoints=false&swLat=63.98590428554631&swLng=-22.784883762939444&neLat=64.44052476295313&neLng=-20.442049290283194&_=1474217065177',
+  // 'https://pokemap.haukur.io/raw_data?pokemon=true&pokestops=false&gyms=false&scanned=false&spawnpoints=false&swLat=64.07643930614307&swLng=-22.145296709082004&neLat=64.19042456941561&neLng=-21.55958809091794&_=1473462800117',
+  // 'http://pogomap.1337.is/raw_data?pokemon=true&pokestops=false&gyms=false&scanned=false&spawnpoints=false&swLat=63.98590428554631&swLng=-22.784883762939444&neLat=64.44052476295313&neLng=-20.442049290283194&_=1474217065177',
   'http://pokekort.hunda.io/raw_data?pokemon=true&pokestops=true&gyms=false&scanned=false&spawnpoints=false&swLat=64.09109777360946&swLng=-21.993753154541082&neLat=64.14811883077199&neLng=-21.70089884545905&_=1473462863293'
   ];
 
